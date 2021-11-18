c7c7c52a4326e68e304397f8266ce5a1fdbb623e
Vyacheslav <vyacheslavburyy@gmail.com>
2021-11-18 18:41:03 +0200

    162. Store enhancers

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,28 @@
-import { createStore, combineReducers } from 'redux';
+import {createStore, combineReducers, compose} from 'redux';
 import filters from "../reducers/filters";
 import heroes from "../reducers/heroes";
 
-const store = createStore(combineReducers({heroes, filters}),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const ondDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === "string") {
+            return ondDispatch({
+                type: action
+            });
+        }
+        return ondDispatch(action);
+    }
+    return store;
+}
+
+const store = createStore(
+    combineReducers({heroes, filters}),
+    compose(
+        enhancer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
 export default store;
